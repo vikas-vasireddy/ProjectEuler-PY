de03014a75c1be355610e47e25207e3d983def99
vikas_vasireddy <vikas.vasireddy@gmail.com>
2016-12-02 01:16:27 -0600

    .

--- Problem1.py
+++ Problem1.py
@@ -1,2 +1,6 @@
-for i in range(10):
- print(i+12)
+def multiples_of_3_or_5():
+    for number in range(1000):
+        if not number %3 or not number % 5:
+            yield number
+print(sum (multiples_of_3_or_5()))
+
