1d99a56333f3ed94fa22cab1f2f607fb73b6e71c
vvasired <vvasired@ford.com>
2018-10-31 14:30:34 -0400

    testing commit from ford network

--- Problem1.py
+++ Problem1.py
@@ -1,5 +1,5 @@
 def multiples_of_3_or_5():
-    for number in range(1000):
+    for number in range(7500):
         if not number %3 or not number % 5:
             yield number
 print(sum (multiples_of_3_or_5()))
